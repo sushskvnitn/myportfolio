278d95c3d3a26c0c80a5ef6a9f560d6150862f73
Sushant Kogurwar <88105162+sushskvnitn@users.noreply.github.com>
2022-03-11 08:48:42 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -34,7 +34,7 @@ function Home() {
           opacity: "50%",
         }}
       />*/}
-      <div
+        <div
         className="card d-flex justify-content-center"
         style={{
           fontFamily: "Roboto Mono, monospace",
@@ -43,7 +43,6 @@ function Home() {
           color: "white",
           background: "none",
           textAlign: "center",
-          zIndex: 1
         }}
       >
         <div className="container py-5" >
@@ -51,7 +50,7 @@ function Home() {
             <h1 style={{}}>
             I'm
               <span
-                style={{ color: "red", fontWeight: "bold", marginLeft: "5px" }}
+                style={{ color: "red", fontWeight: "bold", marginLeft: "10px" }}
               >
                 <Typewriter
                   loop
@@ -66,10 +65,12 @@ function Home() {
             </h1>
           </div>
           <div className="container">
-            <div
-            className="d-none text-center d-flex justify-content-center shadow p-3 mb-5 bg-body rounded">
+            <div 
+            
+            
+            className="row text-center d-flex justify-content-center shadow p-3 mb-5 bg-body rounded">
               <div
-                className="col-xl-3 col-sm-6 mb-5 text-center d-flex justify-content-center"
+                className="col-xl-3 col-sm-6 mb-5"
                 style={{
                   color: "red",
                   marginLeft: "10px",
@@ -118,68 +119,9 @@ function Home() {
                     </li>
                   </ul>
                 </div>
-                </div>
-
+              </div>
             </div>
-      
-
-          <div
-          className="d-none d-sm-block d-md-none  row text-center d-flex justify-content-center shadow p-3 mb-5 bg-body rounded">
-            <div
-              className="col-xl-3 col-sm-6 mb-5"
-              style={{
-                color: "red",
-                marginLeft: "10px",
-                width: "22rem",
-                height: "18rem",
-              }}
-            >
-              <div className="bg-white rounded shadow-sm py-5 px-4">
-                <img
-                  src="https://i.postimg.cc/4dXvghtX/IMG-20211106-214519-447.jpg" data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600"
-                  alt=""
-                  width="130"
-                  className="img-fluid rounded-circle img-thumbnail shadow-sm"
-                />
-                <br />
-                <span className="small text-uppercase text-muted">
-                  sushant kogurwar
-                </span>
-                <ul className="social mb-0 list-inline mt-4">
-                  <li className="list-inline-item">
-                    <a
-                      href="https://www.linkedin.com/in/sushant-kogurwar-2002/"
-                      onClick={() => setProgress(100)}
-                      className="social-link"
-                    >
-                      <FaLinkedin />
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a
-                      href="https://www.instagram.com/sushant_since_2002/"
-                      onClick={() => setProgress(100)}
-                      className="social-link"
-                    >
-                      <FaInstagram />
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a
-                      href="https://github.com/sushskvnitn"
-                      onClick={() => setProgress(100)}
-                      className="social-link"
-                    >
-                      <FaGithub />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              </div>
-
           </div>
-          </div>
-
         </div>
         <div>
       <iframe src="https://embed.lottiefiles.com/animation/83087" title="coder" frameBorder="0"style={{pointerEvents: 'none'}}  ></iframe>
