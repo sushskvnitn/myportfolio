189c594c1a5fc93ca2c0001792d050f3b0bfebce
sushskvnitn <sushantkogurwar@gmail.com>
2024-08-22 22:21:01 +0530

    regular update

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import CoderIntro from "./coderintro";
 import Aos from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 export default function CustomizedTimeline() {
   // const current = new Date();
   // var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -9,35 +9,44 @@ export default function CustomizedTimeline() {
   // const date = `${monthName}  ${current.getFullYear()}`;
 
   useEffect(() => {
-    Aos.init({duration: 2000})
-  }, [])
+    Aos.init({ duration: 2000 });
+  }, []);
   const experience = [
-//     Amazon ML School 2023 - menteeAmazon ML School 2023 - mentee
-// Amazon · ApprenticeshipAmazon · Apprenticeship
-// Sep 2023 - Present · 1 moSep 2023 - Present · 1 mo
-// Remote · RemoteRemote · Remote
-// Exploring machine learning with Amazons ml experts
-{
-      title: "Amazon ML School 2023",
-      description: "Apprenticeship",
-      position : "Mentee",
-      duration: `Sep 2023 - Present `,
+    //     Amazon ML School 2023 - menteeAmazon ML School 2023 - mentee
+    // Amazon · ApprenticeshipAmazon · Apprenticeship
+    // Sep 2023 - Present · 1 moSep 2023 - Present · 1 mo
+    // Remote · RemoteRemote · Remote
+    // Exploring machine learning with Amazons ml experts
+    {
+      title: "Deputy Manager @ICICI Bank ",
+      description: "Full time",
+      position: "Deputy Manager - Information Security ",
+      duration: `July 2024 - Present `,
       details: [
         "Exploring machine learning with Amazons ml experts",
         "Learning about the latest advancements in the field of machine learning and how to apply them to solve real-world problems.",
-      ]
-},
-     {
+      ],
+    }, {
+      title: "Amazon ML School 2023",
+      description: "Apprenticeship",
+      position: "Mentee",
+      duration: `Sep 2023 - Oct 2023`,
+      details: [
+        "Exploring machine learning with Amazons ml experts",
+        "Learning about the latest advancements in the field of machine learning and how to apply them to solve real-world problems.",
+      ],
+    },
+    {
       title: "Freelancer",
       position: "Full stack Developer",
-      "description": "Freelancer.com || Linkedin || Upwork",
-      duration: `Jul 2022 - Present`,
+      description: "Freelancer.com || Linkedin || Upwork",
+      duration: `Jul 2022 - Nov 2023`,
       details: [
         "Developed and maintained code for in-house and client websites utilizing HTML5, CSS3, Javascript, React js, Flask, and Mern Stack.",
         "Tested front-end code in multiple browsers to ensure cross-browser compatibility.",
         "Coordinated closely with other developers and digital producers to expedite projects.",
-        "Clients included from freelancer.com,linkedin ,etc ."
-      ]
+        "Clients included from freelancer.com,linkedin ,etc .",
+      ],
     },
     {
       title: "ACM Student Chapter, VNIT",
@@ -45,11 +54,11 @@ export default function CustomizedTimeline() {
       description: "ACM Student Chapter, VNIT",
       duration: `Jul 2022 - Sep 2023`,
       location: "Nagpur, Maharashtra, India",
-      "details": [
+      details: [
         "Worked on the development of the official website of ACM Student Chapter, VNIT.",
         "Conducted various workshops on Web Development, Competitive Programming, etc.",
-        "Arranged various events like Hackathons, coding contests, etc."
-      ]
+        "Arranged various events like Hackathons, coding contests, etc.",
+      ],
     },
     {
       title: "CSE Student council, VNIT",
@@ -57,31 +66,35 @@ export default function CustomizedTimeline() {
       description: "CSE Student council",
       duration: `Jul 2022 - Sep 2023`,
       location: "Nagpur, Maharashtra, India",
-      "details": [
+      details: [
         "Arranged different events like fresher's party, farewell party,department gatherings, etc.",
         "Worked with the team of 20+ members as a representative.",
-        "Provided guidance to the juniors regarding academics and other activities."
-      ]
-    }
+        "Provided guidance to the juniors regarding academics and other activities.",
+      ],
+    },
   ];
 
   return (
-<section id="about"  style={{paddingBottom:'3rem'}}>
-    <>
-  <CoderIntro/>
-<h1 className="align-center text-centern text-black contact3 font-weight-bold">  Experience </h1>
-<div className="  ">
-   <main class="CV-page">
-            <div class="CV-grid"> <div class="CV-grid-column">
-                  <div class="CV-jobs">
-              {experience.map((exp, index) => (
-               
-                    <section class="CV-timeline CV-job" >
+    <section id="about" style={{ paddingBottom: "3rem" }}>
+      <>
+        <CoderIntro />
+        <h1 className="align-center text-centern text-black contact3 font-weight-bold">
+          {" "}
+          Experience{" "}
+        </h1>
+        <div className="  ">
+          <main class="CV-page">
+            <div class="CV-grid">
+              {" "}
+              <div class="CV-grid-column">
+                <div class="CV-jobs">
+                  {experience.map((exp, index) => (
+                    <section class="CV-timeline CV-job">
                       <h3 class="CV-timeline-heading" data-aos="fade-left">
                         <span class="CV-timeline-heading-title">
                           {exp.title}
                         </span>
-                        <br/>
+                        <br />
                         {exp.position && <span>&#8211;</span>}
                         <span class="CV-timeline-heading-location">
                           {exp.position}
@@ -91,27 +104,27 @@ export default function CustomizedTimeline() {
                         </small>
                       </h3>
                       <ul class="CV-timeline-details" data-aos="fade-left">
-                      {exp.description && <li class="CV-job-timeline-item">{exp.description}</li>}
+                        {exp.description && (
+                          <li class="CV-job-timeline-item">
+                            {exp.description}
+                          </li>
+                        )}
                         {exp.details &&
                           exp.details.map((detail, index) => (
                             <li class="CV-job-timeline-item1">{detail}</li>
                           ))}
                       </ul>
                     </section>
-                
-              ))} 
-             </div>
+                  ))}
                 </div>
+              </div>
             </div>
           </main>
-     
-</div>
-
-</>
-  </section>);
+        </div>
+      </>
+    </section>
+  );
 }
-
-
 
 // import React, { useEffect } from "react";
 // import CoderIntro from "./coderintro";
--- src/components/home.js
+++ src/components/home.js
@@ -1,40 +1,45 @@
 import React, { useEffect } from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
-import {SiLeetcode} from "react-icons/si";
+import { SiLeetcode } from "react-icons/si";
 import { Typewriter } from "react-simple-typewriter";
 import "react-simple-typewriter/dist/index";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import './birdanime.scss';
-// import Wave from 'react-wavify'
-import ani from "./an1.mp4"
+import "./birdanime.scss";
+
 function Home() {
-  
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    link.href =
+      "https://drive.google.com/uc?export=download&id=12X4Zna7yD5DPZ026dkzsJpExE5H2jp9u";
+    link.download = "Sushant_Kogurwar_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
-    <div >
-    
-    <div className="hideonsmallwidth ">
-	<div className="bird-container bird-container--one ">
-		<div className="bird bird--one"></div>
-	</div>
-	
-	<div className="bird-container bird-container--two">
-		<div className="bird bird--two"></div>
-	</div>
-	
-	<div className="bird-container bird-container--three">
-		<div className="bird bird--three"></div>
-	</div>
-	
-	<div className="bird-container bird-container--four">
-		<div className="bird bird--four"></div>
-	</div>
-	
-</div>
+    <div>
+      <div className="hideonsmallwidth ">
+        <div className="bird-container bird-container--one ">
+          <div className="bird bird--one"></div>
+        </div>
+
+        <div className="bird-container bird-container--two">
+          <div className="bird bird--two"></div>
+        </div>
+
+        <div className="bird-container bird-container--three">
+          <div className="bird bird--three"></div>
+        </div>
+
+        <div className="bird-container bird-container--four">
+          <div className="bird bird--four"></div>
+        </div>
+      </div>
 
       <section id="Home" style={{ paddingBottom: "3rem" }}>
         <div
@@ -49,23 +54,46 @@ function Home() {
           }}
         >
           <div className="container py-5">
-            <div data-aos="flip-right"className="d-none d-sm-block ">
-            {/* #37dfa3 */}
-             <h1 className="bounce-in-top" hidden style={{color:"#d6ca4a",paddingLeft:"5rem",marginTop:"2rem",fontWeight:"bold",}}>
-               Sushant Kogurwar
-          
+            <div data-aos="flip-right" className="d-none d-sm-block ">
+              {/* #37dfa3 */}
+              <h1
+                className="bounce-in-top"
+                hidden
+                style={{
+                  color: "#d6ca4a",
+                  paddingLeft: "5rem",
+                  marginTop: "2rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Sushant Kogurwar
               </h1>
-              <div className=" codesx mt-5"  >
-             
-              <iframe src="https://embed.lottiefiles.com/animation/82423"   frameBorder="0" title="animatedicons" scrolling="no"  style={{pointerEvents:"none" ,height:"13rem"}} > </iframe>
-            <a className="btn rounded-circle btn-danger p-1 shake" target="_blank" href="https://linktr.ee/Sushantsk"  rel="noreferrer" >  </a>
+              <div className=" codesx mt-5">
+                <iframe
+                  src="https://embed.lottiefiles.com/animation/82423"
+                  frameBorder="0"
+                  title="animatedicons"
+                  scrolling="no"
+                  style={{ pointerEvents: "none", height: "13rem" }}
+                >
+                  {" "}
+                </iframe>
+                <a
+                  className="btn rounded-circle btn-danger p-1 shake"
+                  target="_blank"
+                  href="https://linktr.ee/Sushantsk"
+                  rel="noreferrer"
+                >
+                  {" "}
+                </a>
               </div>
             </div>
 
             <div data-aos="flip-right" className="d-sm-none">
-             <h1 style={{color:"white"}}>
-              I'm
-                <span className="cursorr"
+              <h1 style={{ color: "white" }}>
+                I'm
+                <span
+                  className="cursorr"
                   style={{
                     color: "#d6ca4a",
                     fontWeight: "bold",
@@ -73,7 +101,7 @@ function Home() {
                   }}
                 >
                   <Typewriter
-                    loop 
+                    loop
                     cursor
                     cursorStyle="|"
                     typeSpeed={70}
@@ -84,10 +112,9 @@ function Home() {
                 </span>
               </h1>
             </div>
-             
 
             <div className="container d-sm-none">
-              <div className="row text-center d-flex justify-content-center  p-3 mb-5  rounded hidden" >
+              <div className="row text-center d-flex justify-content-center  p-3 mb-5  rounded hidden">
                 <div
                   className="col-xl-3 col-sm-6 mb-5 bgmi"
                   style={{
@@ -107,14 +134,11 @@ function Home() {
                       className="img-fluid rounded-circle img-thumbnail -sm"
                     />
                     <br />
-                    <h6 className="small text-uppercase ">
-                      sushant kogurwar
-                    </h6>
+                    <h6 className="small text-uppercase ">sushant kogurwar</h6>
                     <ul className="social mb-0 list-inline mt-4">
                       <li className="list-inline-item">
                         <a
                           href="https://www.linkedin.com/in/sushant-kogurwar"
-                          
                           className="social-link"
                         >
                           <FaLinkedin />
@@ -123,7 +147,6 @@ function Home() {
                       <li className="list-inline-item">
                         <a
                           href="https://www.instagram.com/sushant_since_2002/"
-                          
                           className="social-link"
                         >
                           <FaInstagram />
@@ -132,7 +155,6 @@ function Home() {
                       <li className="list-inline-item">
                         <a
                           href="https://github.com/sushskvnitn"
-                          
                           className="social-link"
                         >
                           <FaGithub />
@@ -141,7 +163,6 @@ function Home() {
                       <li className="list-inline-item">
                         <a
                           href="https://leetcode.com/Kogurwarsushant/"
-                          
                           className="social-link"
                         >
                           <SiLeetcode />
@@ -153,15 +174,20 @@ function Home() {
               </div>
             </div>
           </div>
-          
-   
-          <div style={{height: '100%'}} className="d-none d-sm-block" >
-         
-          <h3 className="  d-flex justify-content-center " data-aos="zoom-in-down">
-          <br /><br /><br /> <br /><br /><br /><br />
-           <div className="w-50"  >
-            <span 
-                >
+
+          <div style={{ height: "100%" }} className="d-none d-sm-block">
+            <h3
+              className="  d-flex justify-content-center "
+              data-aos="zoom-in-down"
+            >
+              <br />
+              <br />
+              <br /> <br />
+              <br />
+              <br />
+              <br />
+              <div className="w-50">
+                <span>
                   <Typewriter
                     loop
                     cursor
@@ -169,12 +195,19 @@ function Home() {
                     typeSpeed={60}
                     deleteSpeed={55}
                     delaySpeed={1000}
-                    words={["Hello , I'm a student and a freelancer as a full stack developer with a focus on technical development.I'm hardworking, diligent, and  dedicated—all qualities I put forward in everything I do"]}
+                    words={[
+                      "Hello , I'm a student and a freelancer as a full stack developer with a focus on technical development.I'm hardworking, diligent, and  dedicated—all qualities I put forward in everything I do",
+                    ]}
                   />
                 </span>
-                </div>
-                
-          </h3>
+              </div>
+            </h3>
+            <button
+              className="btn btn-danger-gradiant mt-3 text-dark border-0 px-3 py-2"
+              onClick={downloadFile}
+            >
+              Download Resume
+            </button>
           </div>
         </div>
       </section>
